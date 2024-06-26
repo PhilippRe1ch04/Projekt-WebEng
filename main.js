dda81f07e5b887b2dde13f7cb05770511767311f
Matze08 <matthias.fauser12@gmail.com>
2024-06-26 17:44:22 +0200

    added character

--- main.js
+++ main.js
@@ -1,25 +1,80 @@
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//init enviroment
+const scene = new THREE.Scene(); //init scene
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); //init cam
+const renderer = new THREE.WebGLRenderer(); //init renderer
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const light = new THREE.AmbientLight(0x404040, 80); // soft white light
+scene.add(light);
+
+const way = new THREE.Mesh(new THREE.PlaneGeometry(3.5, 1000), new THREE.MeshBasicMaterial({color: 0x36454F}));
+way.rotation.x = -Math.PI/2;
+scene.add(way);
+
+const carpet = new THREE.Mesh(new THREE.PlaneGeometry(3, 1000), new THREE.MeshBasicMaterial({color: 0x8b0000}));
+carpet.rotation.x = -Math.PI/2;
+scene.add(carpet);
+
+const loader = new GLTFLoader();
+let mixer;
+let action;
+let action2;
+
+loader.load('public/gentle_stickman.glb', function (gltf) {
+    const model = gltf.scene;
+    model.rotation.y = Math.PI;
+	scene.add(model);
+    mixer = new THREE.AnimationMixer(model);
+    const clips = gltf.animations;
+    action = mixer.clipAction(clips[0]);
+    action2 = mixer.clipAction(clips[1]);
+    action.play();
+
+}, undefined, function (error) {
+
+	console.error(error);
+});
+
+let animationState = 0;
+window.addEventListener('keydown', function(e) {
+    if(e.code === 'ArrowUp'){
+        if(animationState != 1){
+            animationState = 1;
+            action2.reset();
+            action2.play();
+            action.crossFadeTo(action2, 1);
+        } 
+        
+    }
+});
+
+window.addEventListener('keyup', function(e) {
+    if(e.code === 'ArrowUp'){
+        if(animationState != 0){
+            animationState = 0;
+            action.reset();
+            action.play();
+            action.crossFadeFrom(action2, 1);
+        }
+    }
+});
 
 camera.position.z = 5;
+camera.position.y = 5;
+camera.rotation.x = -0.3;
+
+const clock = new THREE.Clock();
 
 function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-
+    if(mixer) {
+        const delta = clock.getDelta();
+        mixer.update(delta/1.5);
+    }
+	renderer.render(scene, camera);
 }
+renderer.setAnimationLoop(animate);
