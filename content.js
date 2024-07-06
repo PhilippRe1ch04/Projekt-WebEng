a165025e483197999d6060a332aff04fe91d7893
Matze08 <matthias.fauser12@gmail.com>
2024-07-06 19:44:22 +0200

    added like counter increase

--- content.js
+++ content.js
@@ -3,10 +3,13 @@ function closeContent(){
     activeScene.addEventListeners();
 }
 
-function like(){
-
+function like(comment){
+    var likeSpans = document.getElementById(comment).getElementsByTagName('span');
+    console.log(likeSpans[0]);
+    var currLikes = likeSpans[0].innerText;
+    likeSpans[0].innerText = parseInt(currLikes) + 1;
 }
 
 function comment(){
-    
+
 }
